b5f08f3db0a69621d94314d67cc7ee9258974ae0
Henry Addo <addhen@linuxmail.org>
2015-06-09 03:38:59 +0900

    Update instructions in the backend

--- i18n/en_US/frontlinecloud.php
+++ i18n/en_US/frontlinecloud.php
@@ -5,22 +5,22 @@ $lang = array(
 		'connect' => 'Connecting FrontlineCloud to this Ushahidi deployment',
 		'step' => 'Step',
 		'create' => 'Configure a new Forward to URL Activity at FrontlineCloud',
-		'create_instructions' => 'You can find the documentation on how to do this on <a href="https://frontlinecloud.zendesk.com/entries/23641986-Creating-a-Web-Connection">FrontlineCloud help desk</a><p>Look for the section <strong>"Configuring a connection to another service"</strong></p>',
-		'configure' => 'Configre Web Connection.',
-		'configure_sender' => 'Enter <strong>s</strong> for Name field',
-		'configure_sender_field' => 'Enter <strong>%s</strong> for value field',
+		'create_instructions' => 'You can find the documentation on how to do this on <a href="https://frontlinecloud.zendesk.com/entries/78296989-Connecting-to-another-web-service-creating-a-Forward-to-URL-Activity">FrontlineCloud help desk</a><p>Look for the section <strong>"Connecting to another web service: creating a Forward to URL Activity"</strong></p>',
+		'configure' => 'Configre Web Connection. The values are case sensitive',
+		'configure_sender' => 'Enter <strong>s</strong> for Key field',
+		'configure_sender_field' => 'Enter <strong>%s</strong> for Value field',
 		'configure_add_param' => 'Add another parameter',
-		'configure_message' => 'Enter m for Name field',
-		'configure_message_field' => 'Enter <strong>%s</strong> for value field',
-		'configure_key' => 'Enter key for Name field',
-		'configure_key_field' => 'Enter <strong>%s</strong> for value field',
+		'configure_message' => 'Enter m for Key field',
+		'configure_message_field' => 'Enter <strong>%s</strong> for Value field',
+		'configure_key' => 'Enter key for Key field',
+		'configure_key_field' => 'Enter <strong>%s</strong> for Value field',
 		'enable_api' => 'Enable API (Expose API)',
 		'secret' => 'Enter <strong>%s</strong> for API secret key',
 		'enter_api_url' => 'Enter the API URL in the field below. Then save.',
 		'enter_url' => 'Enter the URL below for the Upload URL field',
 	),
 	'tooltip' => array(
-		'connect' => 'This synchronizes the messages in the hub with the Ushahidi platform'
+		'connect' => 'This synchronizes the messages in FrontlineCloud with the Ushahidi platform'
 	),
 
 	'frontlinecloud_api_url' => array(
